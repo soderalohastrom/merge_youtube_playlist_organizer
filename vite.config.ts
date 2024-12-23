2422feefaa6a01dfb6ef015df881e457ef90d49b
Scott Soderstrom <ssoderstrom@gmail.com>
2024-12-22 23:55:21 -1000

    update for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env,
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+  };
 });
